833c476b8e99013b9f7a0de7fa5136f3a57389c3
Alfredo Tuesta Abisrror <alfred_2602@outlook.com>
2025-12-03 12:35:30 -0500

    update:tc28

---
+++ Object Repository/Page_falabella.com/svg_Retira hoy_chakra-icon css-n059si.rs
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>svg_Retira hoy_chakra-icon css-n059si</name>
+   <tag></tag>
+   <elementGuidId>3e57f145-e29a-48cf-9892-3110da378ef8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Retira hoy'])[1]/following::*[name()='svg'][4]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>svg.chakra-icon.css-n059si</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>[data-testid=&quot;\31 8673460-menu-button&quot;]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>svg</value>
+      <webElementGuid>27954ecf-3a94-4742-8f22-d9bc7dfc42a7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>viewBox</name>
+      <type>Main</type>
+      <value>0 0 32 32</value>
+      <webElementGuid>b29c13cf-1391-40d2-a844-015900bf2bb4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>focusable</name>
+      <type>Main</type>
+      <value>false</value>
+      <webElementGuid>68ce5390-7e3e-4bd3-8de9-5f59a2741fbb</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>chakra-icon css-n059si</value>
+      <webElementGuid>b9fd42f5-b340-4026-864a-29713856d657</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-hidden</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>546e90f1-56ed-474a-8658-a88d02119942</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;menu-button-2&quot;)/svg[@class=&quot;chakra-icon css-n059si&quot;]</value>
+      <webElementGuid>d18d7abe-d0ad-4003-b54a-d982c6d987cd</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Retira hoy'])[1]/following::*[name()='svg'][4]</value>
+      <webElementGuid>b18f4d09-9b67-4cc9-a070-10744fcf3837</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Llega mañana'])[1]/following::*[name()='svg'][4]</value>
+      <webElementGuid>659a2403-d6f1-4973-b314-465ad6d6fcb8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Agregar'])[1]/preceding::*[name()='svg'][2]</value>
+      <webElementGuid>1e11e9db-204f-4ac1-bada-d05a78c3bbb4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Productos (1)'])[1]/preceding::*[name()='svg'][2]</value>
+      <webElementGuid>636c173f-bf98-451d-af29-d17dc1ddbfb2</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
