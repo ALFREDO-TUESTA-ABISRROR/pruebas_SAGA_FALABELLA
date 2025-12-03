02223bc73bf9e3db5f6683d943f3fa283123695f
Alfredo Tuesta Abisrror <alfred_2602@outlook.com>
2025-12-03 12:48:00 -0500

    tc-30 se valido el campo de departamento,provincia y distrito. (NOTA ERROR INTERNO POR PARTE DEL SISTEMA)

---
+++ Scripts/TC-30_Validar seleccion departamneto,provincia y distrito/Script1764783795876.groovy
@@ -0,0 +1,35 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.falabella.com.pe/falabella-pe')
+
+WebUI.click(findTestObject('Object Repository/Page_Falabella.com  Celebra la Navidad 2025/button_No, gracias-025-53c-f35'))
+
+WebUI.click(findTestObject('Object Repository/Page_Falabella.com  Celebra la Navidad 2025/p_Ingresa tu ubicacin'))
+
+WebUI.click(findTestObject('Object Repository/Page_Falabella.com  Celebra la Navidad 2025/input_Departamento_geofinder-input-level1'))
+
+WebUI.click(findTestObject('Object Repository/Page_Falabella.com  Celebra la Navidad 2025/input_Provincia_geofinder-input-level2'))
+
+WebUI.click(findTestObject('Object Repository/Page_Falabella.com  Celebra la Navidad 2025/input_Distrito_geofinder-input-level3'))
+
+WebUI.closeBrowser()
+
